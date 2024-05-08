184c0260e1cf00dcec47049cdeace90648acd4e4
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-05-08 15:44:13 +0200

    Update genre controller, genre detail and style

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,13 +1,27 @@
 const Genre = require('../models/genre');
+const Album = require('../models/album');
 const asyncHandler = require('express-async-handler');
 
 exports.genre_list = asyncHandler(async (req, res, next) => {
   const allGenres = await Genre.find().sort({ name: 1 }).exec();
-  res.render('genre_list', { title: 'Genre list', genre_list: allGenres });
+  res.render('genre_list', { title: 'Genre List', genre_list: allGenres });
 });
 
 exports.genre_detail = asyncHandler(async (req, res, next) => {
-  res.send(`WiP: Genre detail: ${req.params.id}`);
+  const [genre, albumsInGenre] = await Promise.all([
+    Genre.findById(req.params.id).exec(),
+    Album.find({ genre: req.params.id }, 'title description').exec(),
+  ]);
+  if (genre === null) {
+    const err = new Error('Genre not found');
+    err.status = 404;
+    return next(err);
+  }
+  res.render('genre_detail', {
+    title: 'Genre Detail',
+    genre,
+    genre_albums: albumsInGenre,
+  });
 });
 
 exports.genre_create_get = asyncHandler(async (req, res, next) => {
