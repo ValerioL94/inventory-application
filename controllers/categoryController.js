41a95820a568cb92f28c6aeb10befd57cf25053a
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-05-09 22:09:34 +0200

    Add category update form page

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -99,9 +99,53 @@ exports.category_delete_post = asyncHandler(async (req, res, next) => {
 });
 
 exports.category_update_get = asyncHandler(async (req, res, next) => {
-  res.send('WiP: category update GET');
+  const category = await Category.findById(req.params.id).exec();
+
+  if (category === null) {
+    const err = new Error('Category not found');
+    err.status = 404;
+    return next(err);
+  }
+  res.render('category_form', {
+    title: 'Update Category',
+    category,
+  });
 });
 
-exports.category_update_post = asyncHandler(async (req, res, next) => {
-  res.send('WiP: category update POST');
-});
+exports.category_update_post = [
+  body('name')
+    .trim()
+    .isLength({ min: 3 })
+    .escape()
+    .withMessage('Name must contain at least 3 characters.'),
+  body('description')
+    .optional({ values: 'falsy' })
+    .trim()
+    .isLength({ max: 500 })
+    .escape()
+    .withMessage('Description must contain less than 500 characters.'),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    const category = new Category({
+      name: req.body.name,
+      description: req.body.description,
+      _id: req.params.id,
+    });
+    if (!errors.isEmpty()) {
+      res.render('category_form', {
+        title: 'Update Category',
+        category,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      const updatedCategory = await Category.findByIdAndUpdate(
+        req.params.id,
+        category,
+        {}
+      );
+      res.redirect(updatedCategory.url);
+    }
+  }),
+];
