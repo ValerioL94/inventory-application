77b1d64e013b71d2b775e0d42d3144d33cf4a1ce
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-08-17 15:06:35 +0200

    refactor update function

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,6 @@
 const asyncHandler = require('express-async-handler');
 const { body, validationResult } = require('express-validator');
-const adminPassword = process.env.admin_password;
+const { ADMIN_PASSWORD } = process.env;
 const db = require('../db/queries');
 
 exports.index = asyncHandler(async (req, res, next) => {
@@ -96,6 +96,89 @@ exports.product_create_post = [
     res.redirect('/products');
   }),
 ];
+
+exports.product_update_get = asyncHandler(async (req, res, next) => {
+  const [product, categories] = await Promise.all([
+    db.getProductDetails(req.params.id),
+    db.getCategories(),
+  ]);
+
+  if (product === null) {
+    const err = new Error('Product not found');
+    err.status = 404;
+    return next(err);
+  }
+  res.render('product_form', {
+    title: 'Update Product',
+    product,
+    categories,
+    form_type: 'update',
+  });
+});
+
+exports.product_update_post = [
+  body('name')
+    .trim()
+    .isLength({ min: 2 })
+    .escape()
+    .unescape('&#x27;')
+    .withMessage('Name must contain at least 2 characters'),
+  body('description')
+    .optional({ values: 'falsy' })
+    .trim()
+    .isLength({ max: 500 })
+    .escape()
+    .unescape('&#x27;')
+    .withMessage('Description must contain less than 500 characters'),
+  body('category', 'Category must not be empty')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body(
+    'price',
+    'Minimum price 1, maximum price 100, numbers only, max 2 decimals'
+  )
+    .trim()
+    .isLength({ min: 1 })
+    .toFloat()
+    .isFloat({ min: 0.01, max: 100.99 })
+    .escape(),
+  body(
+    'stock',
+    'Minimum stock quantity 0, maximum stock quantity 100, numbers only'
+  )
+    .trim()
+    .isLength({ min: 1 })
+    .isInt({ min: 0, max: 100 })
+    .escape(),
+  body('password', "Wrong password, You don't have the right.")
+    .trim()
+    .escape()
+    .equals(ADMIN_PASSWORD),
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    const product = {
+      id: req.params.id,
+      name: req.body.name,
+      description: req.body.description,
+      category_id: req.body.category,
+      price: req.body.price,
+      stock: req.body.stock,
+    };
+    if (!errors.isEmpty()) {
+      const categories = db.getCategories();
+      return res.render('product_form', {
+        title: 'Update Product',
+        product,
+        categories,
+        errors: errors.array(),
+        form_type: 'update',
+      });
+    }
+    await db.updateProduct(product);
+    res.redirect('/products');
+  }),
+];
 /*
 exports.product_delete_get = asyncHandler(async (req, res, next) => {
   const product = await Product.findById(req.params.id).exec();
@@ -122,89 +205,6 @@ exports.product_delete_post = [
     }
     await Product.findByIdAndDelete(req.body.productId);
     res.redirect('/inventory/products');
-  }),
-];
-
-exports.product_update_get = asyncHandler(async (req, res, next) => {
-  const [product, allCategories] = await Promise.all([
-    Product.findById(req.params.id).populate('category').exec(),
-    Category.find().sort({ name: 1 }).exec(),
-  ]);
-
-  if (product === null) {
-    const err = new Error('Product not found');
-    err.status = 404;
-    return next(err);
-  }
-  res.render('product_form', {
-    title: 'Update Product',
-    product,
-    categories: allCategories,
-    form_type: 'update',
-  });
-});
-
-exports.product_update_post = [
-  body('name')
-    .trim()
-    .isLength({ min: 2 })
-    .escape()
-    .unescape('&#x27;')
-    .withMessage('Name must contain at least 2 characters'),
-  body('description')
-    .optional({ values: 'falsy' })
-    .trim()
-    .isLength({ max: 500 })
-    .escape()
-    .unescape('&#x27;')
-    .withMessage('Description must contain less than 500 characters'),
-  body('category', 'Category must not be empty')
-    .trim()
-    .isLength({ min: 1 })
-    .escape(),
-  body('price', 'Minimum price 1, maximum price 100, numbers only')
-    .trim()
-    .isLength({ min: 1 })
-    .isInt({ gt: 0, lt: 101 })
-    .escape(),
-  body(
-    'stock',
-    'Minimum stock quantity 0, maximum stock quantity 100, numbers only'
-  )
-    .trim()
-    .isLength({ min: 1 })
-    .isInt({ gt: 0, lt: 101 })
-    .escape(),
-  body('password', "Wrong password, You don't have the right.")
-    .trim()
-    .escape()
-    .equals(adminPassword),
-  asyncHandler(async (req, res, next) => {
-    const errors = validationResult(req);
-    const product = new Product({
-      name: req.body.name,
-      description: req.body.description,
-      category: req.body.category,
-      price: req.body.price,
-      stock: req.body.stock,
-      _id: req.params.id,
-    });
-    if (!errors.isEmpty()) {
-      const allCategories = await Category.find().sort({ name: 1 }).exec();
-      return res.render('product_form', {
-        title: 'Update Product',
-        categories: allCategories,
-        product,
-        errors: errors.array(),
-        form_type: 'update',
-      });
-    }
-    const updatedProduct = await Product.findByIdAndUpdate(
-      req.params.id,
-      product,
-      {}
-    );
-    res.redirect(updatedProduct.url);
   }),
 ];
 
