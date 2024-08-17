41d690c1b3d498770b5563de5b6f33b871a4ec35
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-08-17 15:12:25 +0200

    add delete routers

--- routes/categoryRouter.js
+++ routes/categoryRouter.js
@@ -7,10 +7,8 @@ router.get('/create', category_controller.category_create_get);
 router.post('/create', category_controller.category_create_post);
 router.get('/:id/update', category_controller.category_update_get);
 router.post('/:id/update', category_controller.category_update_post);
-/*
 router.get('/:id/delete', category_controller.category_delete_get);
 router.post('/:id/delete', category_controller.category_delete_post);
-*/
 router.get('/:id', category_controller.category_detail);
 
 module.exports = router;
--- routes/productRouter.js
+++ routes/productRouter.js
@@ -7,10 +7,8 @@ router.get('/create', product_controller.product_create_get);
 router.post('/create', product_controller.product_create_post);
 router.get('/:id/update', product_controller.product_update_get);
 router.post('/:id/update', product_controller.product_update_post);
-/*
 router.get('/:id/delete', product_controller.product_delete_get);
 router.post('/:id/delete', product_controller.product_delete_post);
-*/
 router.get('/:id', product_controller.product_detail);
 
 module.exports = router;
